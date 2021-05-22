6619ce8d88574ee963b064a7da95c9e5e8d14b1f
AmyMegan <amycostall@outlook.com>
2021-05-22 18:55:13 +0100

    Updated with Github link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ export default function App() {
       <h1>
       Weather App 
         </h1>
+        <footer>
+          This was coded by Amy Megan and is {" "}
+        <a 
+        href="https://github.com/AmyMegan"
+        rel="noreferrer"
+        target="_blank"> 
+        open-sourced
+        </a>
+        </footer>
+
     </div>
   );
 }
